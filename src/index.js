310e661c0eca449447b3a548f7725c8a61b51afe
aspalvieri <alex.spalvieri@hotmail.com>
2019-07-08 01:05:10 -0400

    push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Welcome extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = {
+        name: props.name
+      }
+    }
+  
+    render() {
+      return(
+        <div>
+          <p>Hello, {this.state.name}</p>
+  
+          <input value={this.state.name} onChange={(e)=> this.setState({name: e.target.value})} />
+        </div>
+      )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App () {
+  return (
+    <div>
+      <Welcome name="Shaun" />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
